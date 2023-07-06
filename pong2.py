b1fcf3cf64b353f62e1df4ee69e5e3f6ae948492
Kyo jun Hwang <shane5969@naver.com>
2023-07-06 15:21:01 +0900

    ball to player

--- pong2.py
+++ pong2.py
@@ -119,8 +119,8 @@ class Simulate:
     def update_obj(self):
         # update position of all simulation objects using obj.goto(posx, posy)
         self.ball.goto(self.pong.ball.pos[0],self.pong.ball.pos[1])
-        self.playerL.goto(self.pong.playerL.pos[0],self.playerL.ball.pos[1])
-        self.playerR.goto(self.pong.playerR.pos[0],self.playerR.ball.pos[1])
+        self.playerL.goto(self.pong.playerL.pos[0],self.pong.playerL.pos[1])
+        self.playerR.goto(self.pong.playerR.pos[0],self.pong.playerR.pos[1])
         self.window.update()
 
 
